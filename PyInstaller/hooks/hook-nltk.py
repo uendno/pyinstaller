6fa1f029f465fae4aadccf58cedbf541177fe767
Thang Tran <uendno@gmail.com>
2019-12-06 07:29:12 +0700

    fix: Remove error data folder for nltk

--- PyInstaller/hooks/hook-nltk.py
+++ PyInstaller/hooks/hook-nltk.py
@@ -13,10 +13,10 @@ import nltk
 from PyInstaller.utils.hooks import collect_data_files
 
 # add datas for nltk
-datas = collect_data_files('nltk', False)
+# datas = collect_data_files('nltk', False)
 
 # loop through the data directories and add them
-for p in nltk.data.path:
+# for p in nltk.data.path:
     datas.append((p, "nltk_data"))
 
 # nltk.chunk.named_entity should be included

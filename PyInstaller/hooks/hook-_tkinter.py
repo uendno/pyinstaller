81b1660c080489f3adab8251de0d00cb4bc31f63
Thang Tran <uendno@gmail.com>
2019-12-06 08:54:25 +0700

    fix: tkinter hook

--- PyInstaller/hooks/hook-_tkinter.py
+++ PyInstaller/hooks/hook-_tkinter.py
@@ -184,7 +184,7 @@ def _find_tcl_tk(hook_api):
 
         # _tkinter depends on Tcl/Tk compiled as frameworks.
         path_to_tcl = bins[0][1]
-        if 'Library/Frameworks' in path_to_tcl:
+        if 'Library/Frameworks' in path_to_tcl and 'Python' not in path_to_tcl:
             tcl_tk = _find_tcl_tk_darwin_frameworks(bins)
         # Tcl/Tk compiled as on Linux other Unixes.
         # For example this is the case of Tcl/Tk from macports.
